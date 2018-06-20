87b0d39d59f2fa4827a92be20edded5673bb6388
AlexanderMv <postalforever1988@gmail.com>
2018-06-20 20:36:06 +0300

    project started from RabbitEmployeeUI

---
+++ README.md
@@ -0,0 +1,2 @@
+# RabbitEmployeeUI
+# RabbitChatUI
---
+++ src/main/java/org/ChatUI/EmployeeUiApplication.java
@@ -0,0 +1,13 @@
+package org.ChatUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmployeeUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeUiApplication.class, args);
+	}
+}
